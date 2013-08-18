44a8de1b6d1ee7e57c7da5e7a43f2ea4f516af62
David Kantun <dkantun@gmail.com>
2013-08-18 00:19:21 -0500

    Arreglado un error en  en el archivo base.php

--- base.php
+++ base.php
@@ -12,6 +12,8 @@ $gargablog_server = ''; // Sin espacios y sin http://www
 $with_www = 1;
 if($with_www==1)
 $path = 'www.'.$gargablog_server;
+else
+$path = $gargablog_server;
 $folder = '/'; // Dirección donde se encuentra el blog. Si está en la raíz, poner / 
 
 ?>
