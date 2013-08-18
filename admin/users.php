7387e18eb94233a15c29e5817c0e990b55127c31
David Kantun <dkantun@gmail.com>
2013-08-18 14:27:26 -0500

    Iconos para los usuarios

--- admin/users.php
+++ admin/users.php
@@ -145,10 +145,10 @@ else
 echo '<tr>';
 }
   echo '<td><strong>'.$title.'</strong></td>
-  <td><button onClick="window.location.href=\'profile.php?id='.$row['id'].'\'"><img src="icons/Actions-document-preview-archive-icon.png" /></button></td>';
+  <td><button class="button secondary" onClick="window.location.href=\'profile.php?id='.$row['id'].'\'"><img src="icons/Actions-document-preview-archive-icon.png" /></button></td>';
 if($usuarios['group']==1)
-echo ' <td><button onClick="window.location.href=\'edit_profile.php?id='.$row['id'].'\'"><img src="icons/Actions-document-save-as-icon.png" /></button></td>
- <td><button onClick="window.location.href=\'admin.php?id=deleteuser&amp;u='.$row['id'].'\'"><img src="icons/Actions-dialog-close-icon.png" /></button></td>';
+echo ' <td><button class="button secondary" onClick="window.location.href=\'edit_profile.php?id='.$row['id'].'\'"><img src="icons/Actions-document-save-as-icon.png" /></button></td>
+ <td><button class="button secondary" onClick="window.location.href=\'admin.php?id=deleteuser&amp;u='.$row['id'].'\'"><img src="icons/Actions-dialog-close-icon.png" /></button></td>';
 echo '</tr>';
   $count++;
   $contador++;
