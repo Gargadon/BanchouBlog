6bf9ee1aa7132cec1e43045eb88fb6622335a17f
David Kantun <dkantun@gmail.com>
2013-09-20 20:03:00 -0500

    GargaBlog 0.4.4

--- upgrade-from-0.4.x-to-0.4.4.php
+++ upgrade-from-0.4.x-to-0.4.4.php
@@ -13,7 +13,7 @@ mysql_query("CREATE TABLE IF NOT EXISTS `blog_pages` (
   `page` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT");
-mysql_query("UPDATE `blog_config` SET  `version` = '0.4.3'");
+mysql_query("UPDATE `blog_config` SET  `version` = '0.4.4'");
 echo '<p>Se ha actualizado correctamente su base de datos. Por favor, borre este archivo antes de continuar.</p>
 </body></html>';
 ?>
