482057a7f1f049ef3993ca923f149c290f3d396c
David Kantun <dkantun@gmail.com>
2013-09-21 00:25:02 -0500

    GargaBlog 0.4.7
    CKEditor from install wizard, not required to download

--- install/step2.php
+++ install/step2.php
@@ -3,23 +3,35 @@
 
 echo '<div class="small-12 large-12 columns">
 <h3>Instalación de GargaBlog</h3>';
-$ckeditor = 'http://www.gargadon.info/files/ckeditor-for-gargablog.tar.gz';
-$archivo = 'ckeditor-for-gargablog.tar.gz';
-file_put_contents('.$archivo.', file_get_contents('.$ckeditor.'));
-if(file_exists($archivo))
+$ckeditor = 'http://www.gargadon.info/gargablog/files/ckeditor-for-gargablog.tar.gz';
+file_put_contents('ckeditor-for-gargablog.tar.gz', file_get_contents($ckeditor));
+if(file_exists('ckeditor-for-gargablog.tar.gz'))
 {
-	if(md5_file($archivo) == '1ec4035bb6505768dc7099f0c2003c91')
+	if(md5_file('ckeditor-for-gargablog.tar.gz') == '1ec4035bb6505768dc7099f0c2003c91')
 	{
-		$p = new PharData($archivo);
+		$p = new PharData('ckeditor-for-gargablog.tar.gz');
 		$p -> decompress();
-		$phar = new PharData($archivo.'.tar');
-		$phar -> extractTo('.');
-		echo '<p>Se ha instalado exitosamente CKEditor.</p>
-		<p>Para continuar, oprima el botón "Siguiente" para instalar las tablas.</p>	
-		<form action="install.php?step=3" method="post">
-		<input type="hidden" name="instala" value="1" />
-		<input type="submit" value="Siguiente" class="button secondary">
-		</form>';
+		if(file_exists('ckeditor-for-gargablog.tar'))
+		{
+			$phar = new PharData('ckeditor-for-gargablog.tar');
+			$phar -> extractTo('.');
+			unlink('ckeditor-for-gargablog.tar.gz');
+			unlink('ckeditor-for-gargablog.tar');
+			echo '<p>Se ha instalado exitosamente CKEditor.</p>
+			<p>Para continuar, oprima el botón "Siguiente" para instalar las tablas.</p>	
+			<form action="install.php?step=3" method="post">
+			<input type="hidden" name="instala" value="1" />
+			<input type="submit" value="Siguiente" class="button secondary">
+			</form>';
+		}
+		else
+		{
+			echo '<p>No se pudo descomprimir CKEditor.</p>
+			<p>Por favor, habilite la extensión PHAR y ponga la variable <em>phar.readonly</em> en <em>0</em> en su archivo <strong>php.ini</strong></p>
+			<p>Si por alguna razón no puede modificar dicho archivo, consulte a su administrador del servidor.</p>
+			<p>En caso contrario, todavía puede instalar CKEditor de forma manual, descargándolo <a href="'.$ckeditor.'" target="_blank">aquí</a>.
+			Extraiga el .tar.gz y coloque la carpeta <em>ckeditor</em> en el directorio principal de GargaBlog.</p>';
+		}
 	}
 	else
 	{
@@ -28,7 +40,9 @@ if(file_exists($archivo))
 }
 else
 {
-echo '<p>El archivo no pudo ser descargado. Por favor revisa que tengas los permisos correspondientes en el directorio de instalación.</p>
-<p>Si no puedes modificar los permisos del directorio, comunícate con el administrador del servidor.</p>';
+echo '<p>El archivo no pudo ser descargado. Por favor revise que tenga los permisos correspondientes en el directorio de instalación.</p>
+<p>Si no puede modificar los permisos del directorio, comuníquese con el administrador del servidor.</p>
+<p>En caso contrario, todavía puede instalar CKEditor de forma manual, descargándolo <a href="'.$ckeditor.'" target="_blank">aquí</a>.
+Extraiga el .tar.gz y coloque la carpeta <em>ckeditor</em> en el directorio principal de GargaBlog.</p>';
 }
 ?>
