9f037425cb24205cf9454f38d69d4152987276f1
David Kantun <dkantun@gmail.com>
2013-11-15 23:29:17 -0600

    Migration to lang files

---
+++ lang/es_la.php
@@ -0,0 +1,37 @@
+<?php
+/* ¿Como va a funcionar?
+
+	$texto es la variable a la que llamará
+	todo el software. Para cambiar el idioma,
+	puede cambiarlo en la tabla blog_config.
+	
+	No borre lang/es_la.php. Es el archivo de
+	respaldo para el resto de idiomas sin
+	traducir al 100%.
+*/
+// Barra de navegacion
+$texto['1']='Administración';
+$texto['2']='Categorías';
+$texto['3']='Páginas';
+$texto['4']='Buscar';
+$texto['5']='Login';
+$texto['6']='Logout';
+$texto['7']='Configurar blog';
+$texto['8']='Configurar categorías';
+$texto['9']='Configurar páginas';
+$texto['10']='Editar entradas';
+$texto['11']='Nueva entrada';
+$texto['12']='Crear usuarios';
+$texto['13']='Modificar usuarios';
+//Página principal
+$texto['14']='Mostrando página';
+$texto['15']='de';
+$texto['16']='Esto está muy vacío.';
+$texto['17']='¿Por qué no escribes tu primer entrada?';
+$texto['18']='Anterior';
+$texto['19']='Siguiente';
+$texto['20']='Primero';
+$texto['21']='Último';
+// Vista de las entradas
+
+?>
