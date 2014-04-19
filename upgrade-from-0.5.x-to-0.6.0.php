f28dfe4793a6d0831f98944bc4495b44a24148fb
David Kantun <dkantun@gmail.com>
2014-04-19 00:13:24 -0500

    GargaBlog 0.6.0 Beta 2

--- upgrade-from-0.5.x-to-0.6.0.php
+++ upgrade-from-0.5.x-to-0.6.0.php
@@ -4,7 +4,7 @@ require('db.php');
 echo '<html lang="es-mx">
   <head><meta content="text/html; charset=UTF-8" http-equiv="content-type" /></head>
 <body>
-<p>Este archivo actualizará su base de datos para utilizar la versión 0.6.0a2 de GargaBlog.</p>';
+<p>Este archivo actualizará su base de datos para utilizar la versión 0.6.0b1 de GargaBlog.</p>';
 mysql_query("CREATE TABLE IF NOT EXISTS `blog_pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `author` int(11) NOT NULL,
@@ -14,7 +14,7 @@ mysql_query("CREATE TABLE IF NOT EXISTS `blog_pages` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8");
 mysql_query("ALTER TABLE  `blog_config` ADD `lang` TEXT NOT NULL");
-mysql_query("UPDATE `blog_config` SET  `version` = '0.6.0a2'");
+mysql_query("UPDATE `blog_config` SET  `version` = '0.6.0b1'");
 mysql_query("UPDATE `blog_config` SET  `lang` = 'es'");
 echo '<p>Se ha actualizado correctamente su base de datos. Por favor, borre este archivo antes de continuar.</p>
 </body></html>';
